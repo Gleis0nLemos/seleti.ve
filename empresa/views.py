349355adc6e6deba875a5b2b698fc0440dceafc2
Gleison <gleison04lemos@gmail.com>
2022-11-08 00:17:49 -0300

    feat: finish structures and function delete

--- empresa/views.py
+++ empresa/views.py
@@ -52,3 +52,9 @@ def empresas(request):
 
     empresas = Empresa.objects.all()
     return render(request, 'empresa.html', {'empresas': empresas})
+
+def excluir_empresa(request, id):
+    empresa = Empresa.objects.get(id=id)
+    empresa.delete()
+    messages.add_message(request, constants.SUCCESS, 'Empresa excluída com sucesso')
+    return redirect('/home/empresas')
